2034908ba31b52d1cbf282d39388058b1ba2dd76
lamstutz <louis.amstutz@gmail.com>
2018-03-14 13:49:43 +0100

    pathfindig v2

---
+++ Assets/lamstutz/CopTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CopTrigger : MonoBehaviour {
+
+	public UnityEvent actionOnTrigger;
+
+	private void OnTriggerEnter2D (Collider2D other) {
+		if (actionOnTrigger != null) actionOnTrigger.Invoke ();
+	}
+}
--- Assets/lamstutz/scripts/OnClickFindPathAndMove.cs
+++ Assets/lamstutz/scripts/OnClickFindPathAndMove.cs
@@ -8,8 +8,8 @@ using UnityEngine;
 public class OnClickFindPathAndMove : MonoBehaviour {
 
 
-	public Transform objectToMove;
-	public Transform targetObject;
+	private Transform objectToMove;
+	private Transform targetObject;
 	public Collider2D pathCollider;
 
 	private Vector3? moveTo;
@@ -18,6 +18,8 @@ public class OnClickFindPathAndMove : MonoBehaviour {
 	private Grider grid;
 	private Vector2 currentTarget;
 	private Vector2 targetV2;
+	private bool started = false;
+	private bool ended = false;
 
 	void Start() { 
 		print("START PATHFINDING");
@@ -26,45 +28,48 @@ public class OnClickFindPathAndMove : MonoBehaviour {
 	}
 
 	void Update() {
-		targetV2 = new Vector2(targetObject.position.x,targetObject.position.y);
-		if (
-			grid != null && 
-			(currentTarget == null || (Vector2.Distance(targetV2, currentTarget) > 2.0f ))
-		) {
-			fin = false;
-			Vector3 origin = objectToMove.position;
-			var v3 = targetObject.position;
-			v3.z = 10.0f;
-			Vector3 target =  v3;
-			
-			var findedPath = AStar
-				.FindPath(grid.WorldToCell(origin), grid.WorldToCell(target), Collide,targetV2,currentTarget);
-
-			if(findedPath != null){
-				currentPath.nodes = findedPath.Select(pos => (Vector3) grid.CellToWorld(pos))
-				.ToList<Vector3>();
-			}
-			currentTarget = new Vector2(target.x, target.y);
-
+		if(started && !ended){
+			targetV2 = new Vector2(targetObject.position.x,targetObject.position.y);
+			if (
+				grid != null && 
+				(currentTarget == null || (Vector2.Distance(targetV2, currentTarget) > 2.0f ))
+			) {
+				fin = false;
+				Vector3 origin = objectToMove.position;
+				var v3 = targetObject.position;
+				v3.z = 10.0f;
+				Vector3 target =  v3;
 				
-			moveTo = null;
-		}
-		if (!fin && moveTo.HasValue && Vector2.Distance(moveTo.Value, objectToMove.position) < 0.1f) {
-			moveTo = null;
-		}
-		if (currentPath.nodes.Count > 0) {
-			if (!moveTo.HasValue) {
-				moveTo = currentPath.nodes[0];
-				currentPath.nodes.RemoveAt(0);
-			}
-		}
-		if (moveTo.HasValue) {
-			this.objectToMove.position = Vector2.MoveTowards(this.objectToMove.position, moveTo.Value, 0.05f);
-		}
+				var findedPath = AStar
+					.FindPath(grid.WorldToCell(origin), grid.WorldToCell(target), Collide,targetV2,currentTarget);
 
-		if(!fin && currentPath.nodes.Count == 0 && (currentTarget != null || (Vector2.Distance(targetV2, currentTarget) <= 2.0f ) )){
-			fin = true;
-			print("FINISH PATHFINDING");
+				if(findedPath != null){
+					currentPath.nodes = findedPath.Select(pos => (Vector3) grid.CellToWorld(pos))
+					.ToList<Vector3>();
+				}
+				currentTarget = new Vector2(target.x, target.y);
+
+					
+				moveTo = null;
+			}
+			if (!fin && moveTo.HasValue && Vector2.Distance(moveTo.Value, objectToMove.position) < 0.1f) {
+				moveTo = null;
+			}
+			if (currentPath.nodes.Count > 0) {
+				if (!moveTo.HasValue) {
+					moveTo = currentPath.nodes[0];
+					currentPath.nodes.RemoveAt(0);
+				}
+			}
+			if (moveTo.HasValue) {
+				this.objectToMove.position = Vector2.MoveTowards(this.objectToMove.position, moveTo.Value, 0.05f);
+			}
+
+			if(!fin && currentPath.nodes.Count == 0 && (currentTarget != null || (Vector2.Distance(targetV2, currentTarget) <= 2.0f ) )){
+				fin = true;
+				//ended = true;
+				print("FINISH PATHFINDING");
+			}
 		}
 	}
 
@@ -73,6 +78,14 @@ public class OnClickFindPathAndMove : MonoBehaviour {
         if (pathCollider == null) return false;
 			return pathCollider.OverlapPoint(grid.CellToWorld(cellPos));
     }
+
+
+	public void startPath(string str){
+		string[] toMoveTarget = str.Split('|');
+		objectToMove = GameObject.Find(toMoveTarget[0]).GetComponent<Transform>();
+		targetObject = GameObject.Find(toMoveTarget[1]).GetComponent<Transform>();
+		started = true;
+	}
 
 
 }

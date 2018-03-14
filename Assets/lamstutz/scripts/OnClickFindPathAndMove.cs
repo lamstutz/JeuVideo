9a5d3f3c3b5ffe7d659f0fef08320d5aeaa410ee
lamstutz <louis.amstutz@gmail.com>
2018-03-14 23:02:05 +0100

    add callback in pathfinding

--- Assets/lamstutz/scripts/OnClickFindPathAndMove.cs
+++ Assets/lamstutz/scripts/OnClickFindPathAndMove.cs
@@ -20,6 +20,8 @@ public class OnClickFindPathAndMove : MonoBehaviour {
 	private Vector2 targetV2;
 	private bool started = false;
 	private bool ended = false;
+	private string callback = "";
+	private string objectToMoveName = "";
 
 	void Start() { 
 		print("START PATHFINDING");
@@ -30,10 +32,9 @@ public class OnClickFindPathAndMove : MonoBehaviour {
 	void Update() {
 		if(started && !ended){
 			targetV2 = new Vector2(targetObject.position.x,targetObject.position.y);
-			if (
-				grid != null && 
-				(currentTarget == null || (Vector2.Distance(targetV2, currentTarget) > 2.0f ))
-			) {
+
+			//pathfinding 
+			if (grid != null && (currentTarget == null || (Vector2.Distance(targetV2, currentTarget) > 2.0f ))) {
 				fin = false;
 				Vector3 origin = objectToMove.position;
 				var v3 = targetObject.position;
@@ -52,23 +53,35 @@ public class OnClickFindPathAndMove : MonoBehaviour {
 					
 				moveTo = null;
 			}
+
+			//arrivé au noeud courant
 			if (!fin && moveTo.HasValue && Vector2.Distance(moveTo.Value, objectToMove.position) < 0.1f) {
 				moveTo = null;
 			}
+
+			//prise en compte du prochain noeud
 			if (currentPath.nodes.Count > 0) {
 				if (!moveTo.HasValue) {
 					moveTo = currentPath.nodes[0];
 					currentPath.nodes.RemoveAt(0);
 				}
 			}
+
+			//deplacement
 			if (moveTo.HasValue) {
 				this.objectToMove.position = Vector2.MoveTowards(this.objectToMove.position, moveTo.Value, 0.05f);
 			}
 
+			//arrivé à la cible
 			if(!fin && currentPath.nodes.Count == 0 && (Vector2.Distance(targetV2, currentTarget) <= 2.0f ) ){
 				fin = true;
 				ended = true;
 				print("FINISH PATHFINDING");
+
+				if(callback != null && callback == "dialogue"){
+					GameObject.Find(objectToMoveName).GetComponent<DialogueTrigger>().TriggerDialogue();
+				}
+
 			}
 		}
 	}
@@ -82,9 +95,16 @@ public class OnClickFindPathAndMove : MonoBehaviour {
 
 	public void startPath(string str){
 		string[] toMoveTarget = str.Split('|');
-		objectToMove = GameObject.Find(toMoveTarget[0]).GetComponent<Transform>();
+		objectToMoveName = toMoveTarget[0];
+		objectToMove = GameObject.Find(objectToMoveName).GetComponent<Transform>();
 		targetObject = GameObject.Find(toMoveTarget[1]).GetComponent<Transform>();
+		if(toMoveTarget.Length > 2){
+			callback = toMoveTarget[2];
+		}else{
+			callback = "";
+		}
 		started = true;
+		ended = false;
 	}
 
 

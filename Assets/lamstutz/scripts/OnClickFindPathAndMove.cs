70420b66228879d9a9c467cc32609d77e261fdb6
lamstutz <louis.amstutz@gmail.com>
2018-03-14 17:25:22 +0100

    test 1 multi thread

--- Assets/lamstutz/scripts/OnClickFindPathAndMove.cs
+++ Assets/lamstutz/scripts/OnClickFindPathAndMove.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using pathfinding;
 using UnityEngine;
 
+//using CielaSpike;
+using System.Threading;
+
+
 public class OnClickFindPathAndMove : MonoBehaviour {
 
 
@@ -21,6 +25,18 @@ public class OnClickFindPathAndMove : MonoBehaviour {
 	private bool started = false;
 	private bool ended = false;
 
+
+
+
+
+	Thread ChildThread = null;
+    EventWaitHandle ChildThreadWait = new EventWaitHandle(true, EventResetMode.ManualReset);
+    EventWaitHandle MainThreadWait = new EventWaitHandle(true, EventResetMode.ManualReset);
+
+
+
+
+
 	void Start() { 
 		print("START PATHFINDING");
 		grid = GameObject.Find("/trackerMove/grid").GetComponent<Grider>();
@@ -29,47 +45,7 @@ public class OnClickFindPathAndMove : MonoBehaviour {
 
 	void Update() {
 		if(started && !ended){
-			targetV2 = new Vector2(targetObject.position.x,targetObject.position.y);
-			if (
-				grid != null && 
-				(currentTarget == null || (Vector2.Distance(targetV2, currentTarget) > 2.0f ))
-			) {
-				fin = false;
-				Vector3 origin = objectToMove.position;
-				var v3 = targetObject.position;
-				v3.z = 10.0f;
-				Vector3 target =  v3;
-				
-				var findedPath = AStar
-					.FindPath(grid.WorldToCell(origin), grid.WorldToCell(target), Collide,targetV2,currentTarget);
-
-				if(findedPath != null){
-					currentPath.nodes = findedPath.Select(pos => (Vector3) grid.CellToWorld(pos))
-					.ToList<Vector3>();
-				}
-				currentTarget = new Vector2(target.x, target.y);
-
-					
-				moveTo = null;
-			}
-			if (!fin && moveTo.HasValue && Vector2.Distance(moveTo.Value, objectToMove.position) < 0.1f) {
-				moveTo = null;
-			}
-			if (currentPath.nodes.Count > 0) {
-				if (!moveTo.HasValue) {
-					moveTo = currentPath.nodes[0];
-					currentPath.nodes.RemoveAt(0);
-				}
-			}
-			if (moveTo.HasValue) {
-				this.objectToMove.position = Vector2.MoveTowards(this.objectToMove.position, moveTo.Value, 0.05f);
-			}
-
-			if(!fin && currentPath.nodes.Count == 0 && (currentTarget != null || (Vector2.Distance(targetV2, currentTarget) <= 2.0f ) )){
-				fin = true;
-				//ended = true;
-				print("FINISH PATHFINDING");
-			}
+			pather();
 		}
 	}
 
@@ -79,6 +55,48 @@ public class OnClickFindPathAndMove : MonoBehaviour {
 			return pathCollider.OverlapPoint(grid.CellToWorld(cellPos));
     }
 
+	private void pather(){
+		targetV2 = new Vector2(targetObject.position.x,targetObject.position.y);
+		if (
+			grid != null && 
+			(currentTarget == null || (Vector2.Distance(targetV2, currentTarget) > 2.0f ))
+		) {
+			Vector3 origin = objectToMove.position;
+			var v3 = targetObject.position;
+			v3.z = 10.0f;
+			Vector3 target =  v3;
+			
+			var findedPath = AStar
+				.FindPath(grid.WorldToCell(origin), grid.WorldToCell(target), Collide,targetV2,currentTarget);
+
+			if(findedPath != null){
+				currentPath.nodes = findedPath.Select(pos => (Vector3) grid.CellToWorld(pos))
+				.ToList<Vector3>();
+			}
+			currentTarget = new Vector2(target.x, target.y);
+			fin = false;				
+			moveTo = null;
+		}
+		if (!fin && moveTo.HasValue && Vector2.Distance(moveTo.Value, objectToMove.position) < 0.1f) {
+			moveTo = null;
+		}
+		if (currentPath.nodes.Count > 0) {
+			if (!moveTo.HasValue) {
+				moveTo = currentPath.nodes[0];
+				currentPath.nodes.RemoveAt(0);
+			}
+		}
+		if (moveTo.HasValue) {
+			this.objectToMove.position = Vector2.MoveTowards(this.objectToMove.position, moveTo.Value, 0.05f);
+		}
+
+		if(!fin && currentPath.nodes.Count == 0 && (currentTarget != null || (Vector2.Distance(targetV2, currentTarget) <= 2.0f ) )){
+			fin = true;
+			//ended = true;
+			print("FINISH PATHFINDING");
+		}
+	}
+
 
 	public void startPath(string str){
 		string[] toMoveTarget = str.Split('|');
@@ -86,6 +104,42 @@ public class OnClickFindPathAndMove : MonoBehaviour {
 		targetObject = GameObject.Find(toMoveTarget[1]).GetComponent<Transform>();
 		started = true;
 	}
+
+
+
+using UnityEngine;
+ 
+public class ThreadedBehaviour : MonoBehaviour
+{
+    
+ 
+    void ChildThreadLoop()
+    {
+        ChildThreadWait.Reset();
+        ChildThreadWait.WaitOne();
+ 
+        while(true)
+        {
+            ChildThreadWait.Reset();
+ 
+            // Do Update
+ 
+            WaitHandle.SignalAndWait(MainThreadWait, ChildThreadWait);
+        }
+    }
+ 
+    void Awake()
+    {
+        ChildThread = new Thread(ChildThreadLoop);
+        ChildThread.Start();
+        }
+ 
+    void Update()
+    {
+        MainThreadWait.Reset();
+        WaitHandle.SignalAndWait(ChildThreadWait, MainThreadWait);
+    }
+}
 
 
 }

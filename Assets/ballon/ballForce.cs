57adc883a11f984c1dbb831a0f6969257e694b9d
lamstutz <louis.amstutz@gmail.com>
2018-03-18 16:14:03 +0100

    ball test 1

---
+++ Assets/ballon/ballForce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (GameObject))]
+public class ballForce : MonoBehaviour {
+
+	private Rigidbody2D ball;
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Rigidbody2D>();
+		ball.AddForce(new Vector2(80,10));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

c2f0392689b5a526e950ec194028564ca53b9185
lamstutz <louis.amstutz@gmail.com>
2018-04-09 23:14:08 +0200

    debut fin

--- Assets/Main/levelEnabler.cs
+++ Assets/Main/levelEnabler.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class levelEnabler : MonoBehaviour {
 
 	private BoxCollider2D collide;
+	//private currentLevel level;
 	// Use this for initialization
 	void Start () {
 		collide = GetComponent<BoxCollider2D>();
+		//level = GetComponent<currentLevel>();
+		collide.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void active(){
+		collide.enabled = true;
+	}
+
+	private void OnTriggerEnter2D (Collider2D objectCol) {
+		//if (level.level > 1) {
+			if (objectCol.name != "barriere" && objectCol.name != "barriereEnd") {
+				active ();
+			}
+		//}
 	}
 }
